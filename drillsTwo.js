b0410e9cf87b4d14883768b48d2804a3dda5640f
nevelson <nevelson92@gmail.com>
2020-03-17 15:39:59 -0400

    Find by id

--- drillsTwo.js
+++ drillsTwo.js
@@ -1,5 +1,5 @@
 //Make student reports
-"use strict";
+'use strict';
 
 function makeStudentReport(data) {
   const resultArray = [];
@@ -11,47 +11,42 @@ function makeStudentReport(data) {
 }
 
 const dataTest = [
-  { name: "john", grade: "b" },
-  { name: "ference", grade: "c" }
+  { name: 'john', grade: 'b' },
+  { name: 'ference', grade: 'c' }
 ];
 
 console.log(makeStudentReport(dataTest));
 
 const studentData = [
   {
-    name: "Tim",
-    status: "Current student",
-    course: "Biology"
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology'
   },
   {
-    name: "Sue",
-    status: "Withdrawn",
-    course: "Mathematics"
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics'
   },
   {
-    name: "Liz",
-    status: "On leave",
-    course: "Computer science"
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science'
   }
 ];
 
 function enrollInSummerSchool(studentData) {
-  studentData.forEach(studentData => (studentData.status = "In summer school"));
+  studentData.forEach(studentData => (studentData.status = 'In summer school'));
   return studentData;
 }
 
-console.log(enrollInSummerSchool(studentData));
+const test = enrollInSummerSchool(studentData);
+test.forEach(x => console.log(x));
 
-//expected output
-// [
-//   {
-//     name: 'Tim',
-//     status: 'In Summer school',
-//     course: 'Biology'
-//   },
-//   {
-//     name: 'Sue',
-//     status: 'In Summer school',
-//     course: 'Mathematics'
-//   },
-// ];
+//Find by id
+const data = [{ id: 1, foo: 'bar' }, { id: 2, foo: 'bizz' }];
+function findById(items, idNum) {
+  return items.filter(item => item.id === idNum);
+}
+
+console.log(findById(data, 2));
