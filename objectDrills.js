5e50a10bdc1d19a09f007d2206d45358aed463a6
Logan Stribling <logan.stribling@gmail.com>
2020-03-17 15:02:34 -0600

    objectDrills 3rd set

--- objectDrills.js
+++ objectDrills.js
@@ -1,13 +1,31 @@
 // 1 object initializers and methods
-'use strict';
+"use strict";
 
 const loaf = {
   flour: 300,
   water: 210,
-  hydration: function () {
+  hydration: function() {
     return (this.water / this.flour) * 100;
   }
 };
 
 console.log(`flour: ${loaf.flour} | hydration: ${loaf.water}`);
 console.log(loaf.hydration());
+
+const myObj = {
+  foo: "fighters",
+  bar: "scuba",
+  fum: "fumes",
+  quux: "quiz",
+  spam: "musabi"
+};
+
+function myKeys(myObj) {
+  for (const key in myObj) {
+    const value = myObj[key];
+    console.log(key);
+    console.log(value);
+  }
+}
+
+myKeys(myObj);
